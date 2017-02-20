d7563994efcac19ce37fbecc68fb1b801be4db0a
«DeveloperArty» <apple_dev.arty@mail.ru>
2017-02-20 14:58:02 +0300

    DAL implemented

--- Currency converter/ViewController.swift
+++ Currency converter/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // MARK: Outlets
     @IBOutlet weak var label: UILabel!
     
     @IBOutlet weak var pickerFrom: UIPickerView!
@@ -18,10 +17,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    // MARK: Properties 
     let currencies = ["RUB", "USD", "EUR"]
-    
-    // MARK: VC Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.label.text = "Тут будет курс"
@@ -30,7 +27,18 @@ class ViewController: UIViewController {
         self.pickerFrom.dataSource = self
         
         self.pickerTo.delegate = self
-        self.pickerFrom.delegate = self 
+        self.pickerFrom.delegate = self
+        
+        self.activityIndicator.hidesWhenStopped = true 
+        
+        self.retreiveCurrencyRate(baseCurrency: "USD", toCurrency: "RUB") { [weak self] (value) in
+            DispatchQueue.main.async(execute: {
+                if let strongSelf = self {
+                    strongSelf.label.text = value
+                }
+            })
+                                    
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,7 +46,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func requestCurrencyRates(baseCurrency: String, pasreHandler: @escaping (Data?, Error?) -> Void) {
+        let url = URL(string: "https://api.fixer.io/latest?base=" + baseCurrency)!
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (dataReceived, response, error) in
+            pasreHandler(dataReceived, error)
+        }
+        
+        dataTask.resume()
+    }
+    
+    func parseCurrencyRatesResponce(data: Data?, toCurrency: String) -> String {
+        var value = ""
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as? Dictionary<String, Any>
+            
+            if let parsedJSON = json {
+                print("\(parsedJSON)")
+                if let rates = parsedJSON["rates"] as? Dictionary<String, Double> {
+                    if let rate = rates[toCurrency] {
+                        value = "\(rate)"
+                    } else {
+                        value = "No rate for currency \"\(toCurrency)\" found"
+                    }
+                } else {
+                    value = "No \"rates\" field found"
+                }
+            } else {
+                value = "No JSON value parsed"
+            }
+        } catch {
+            value = error.localizedDescription
+        }
+        
+        return value
+    }
+    
+    func retreiveCurrencyRate(baseCurrency: String, toCurrency: String, completion: @escaping (String) -> Void) {
+        self.requestCurrencyRates(baseCurrency: baseCurrency) { [weak self] (data, error) in
+            var string = "No currency retrieved!"
+            
+            if let currentError = error {
+                string = currentError.localizedDescription
+            } else {
+                if let strongSelf = self {
+                    string = strongSelf.parseCurrencyRatesResponce(data: data, toCurrency: toCurrency)
+                }
+            }
+            
+            completion(string)
+        }
+    }
 }
 
 // MARK: UIPickerViewDataSource
